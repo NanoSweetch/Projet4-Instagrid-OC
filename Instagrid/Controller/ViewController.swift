b35516dd951618d6a5a14a9a9f08acc84f999a3b
Maxime <maxime.dutour@gmail.com>
2020-03-04 16:39:51 +0100

    Minor changes. - Stackview grid modification - Button code improvement - Added radius of views grid

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,28 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var viewGridTopLeft: UIButton!
-    @IBOutlet weak var viewGridTopRight: UIButton!
-    @IBOutlet weak var viewGridBottomLeft: UIButton!
-    @IBOutlet weak var viewGridBottomRight: UIButton!
     
+    // Outlets View Grid
+    @IBOutlet weak var viewGrid: UIView!
+    @IBOutlet weak var viewGridTopLeft: UIImageView!
+    @IBOutlet weak var viewGridTopRight: UIImageView!
+    @IBOutlet weak var viewGridBottomLeft: UIImageView!
+    @IBOutlet weak var viewGridBottomRight: UIImageView!
     
-  
+    // Outlets Buttons
+    @IBOutlet weak var buttonRectangleTopImg: UIButton!
+    @IBOutlet weak var buttonRectangleBottomImg: UIButton!
+    @IBOutlet weak var buttonSquareImg: UIButton!
     
     
     // Action button
-    
-    @IBAction func buttonRectangleBottom(_ sender: Any) {
-        setStyle(.rectangleBottom)
-    }
-    
     @IBAction func buttonRectangleTop(_ sender: Any) {
-        setStyle(.rectangleTop)
+        style = .rectangleTop
     }
-    
+    @IBAction func buttonRectangleBottom(_ sender: Any) {
+        style = .rectangleBottom
+    }
     @IBAction func buttonSquare(_ sender: Any) {
-        setStyle(.square)
+        style = .square
     }
     
     // Configuration des styles et actions des boutons
@@ -38,42 +39,34 @@ class ViewController: UIViewController {
         case rectangleTop, rectangleBottom, square
     }
     
-//    var style: StyleGrid = .rectangleBottom{
-//        didSet {
-//            setStyle(style)
-//        }
-//    }
+   var style: StyleGrid = .rectangleBottom{
+        didSet {
+            setStyle(style)
+        }
+    }
     
     private func setStyle(_ style: StyleGrid) {
         switch style {
         case .rectangleTop:
             viewGridTopRight.isHidden = true
             viewGridBottomRight.isHidden = false
-            //rationTopLeftView.isActive = false
-            //rationBottomLeftView.isActive = true
-            
-            
-//            buttonGridImageLeft.setBackgroundImage(UIImage(named: "Layout Selected 1"), for: .normal)
-//            buttonGridImageCenter.setBackgroundImage(UIImage(named: "Layout 2"), for: .normal)
-//            buttonGridImageRight.setBackgroundImage(UIImage(named: "Layout 3"), for: .normal)
+            buttonRectangleTopImg.setBackgroundImage(UIImage(named: "Layout Selected 1"), for: .normal)
+            buttonRectangleBottomImg.setBackgroundImage(UIImage(named: "Layout 2"), for: .normal)
+            buttonSquareImg.setBackgroundImage(UIImage(named: "Layout 3"), for: .normal)
           
         case .rectangleBottom:
              viewGridBottomRight.isHidden = true
              viewGridTopRight.isHidden = false
-             //rationTopLeftView.isActive = true
-             //rationBottomLeftView.isActive = false
-//             buttonGridImageLeft.setBackgroundImage(UIImage(named: "Layout 1"), for: .normal)
-//             buttonGridImageCenter.setBackgroundImage(UIImage(named: "Layout Selected 2"), for: .normal)
-//             buttonGridImageRight.setBackgroundImage(UIImage(named: "Layout 3"), for: .normal)
+             buttonRectangleTopImg.setBackgroundImage(UIImage(named: "Layout 1"), for: .normal)
+             buttonRectangleBottomImg.setBackgroundImage(UIImage(named: "Layout Selected 2"), for: .normal)
+             buttonSquareImg.setBackgroundImage(UIImage(named: "Layout 3"), for: .normal)
            
         case .square:
             viewGridTopRight.isHidden = false
             viewGridBottomRight.isHidden = false
-            //rationTopLeftView.isActive = true
-            //rationBottomLeftView.isActive = true
-//            buttonGridImageLeft.setBackgroundImage(UIImage(named: "Layout 1"), for: .normal)
-//            buttonGridImageCenter.setBackgroundImage(UIImage(named: "Layout 2"), for: .normal)
-//            buttonGridImageRight.setBackgroundImage(UIImage(named: "Layout Selected 3"), for: .normal)
+            buttonRectangleTopImg.setBackgroundImage(UIImage(named: "Layout 1"), for: .normal)
+            buttonRectangleBottomImg.setBackgroundImage(UIImage(named: "Layout 2"), for: .normal)
+            buttonSquareImg.setBackgroundImage(UIImage(named: "Layout Selected 3"), for: .normal)
             
             
         }
@@ -82,6 +75,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Configuring the radius of the grid view
+               viewGrid.layer.cornerRadius = 15
+               viewGridTopLeft.layer.cornerRadius = 4
+               viewGridTopRight.layer.cornerRadius = 4
+               viewGridBottomLeft.layer.cornerRadius = 4
+               viewGridBottomRight.layer.cornerRadius = 4
     }
 
 
